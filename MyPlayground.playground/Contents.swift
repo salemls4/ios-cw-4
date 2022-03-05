64926c5994ec987c8d60520b69b5394689fa2687
salemls4 <84942947+salemls4@users.noreply.github.com>
2022-03-05 13:41:50 +0300

    done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+let name = "salem"
+
+var favouriteNumber = 7
+
+var hobby = "video gmaes"
+
+var number1 :Int = 16
+
+var number2 :Double = 16.99
+
+print ("سلام عليكم اسمي\(name)ورقمي المفضل\(favouriteNumber)هوايتي المضلة\(hobby)ورقم الصحيح \(number1)ورقم العشري \(number2)")
